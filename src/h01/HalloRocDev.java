5694a76f2169129d9f4a0ce044b04c8c45ed412a
joey <joey.heijm@student.rocvf.nl>
2018-10-16 11:08:07 +0200

    Added demo.

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,7 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 7A!");
+        System.out.println("Hallo klas 8B" +
+                "!");
     }
 }
